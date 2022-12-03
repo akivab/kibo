37cf0e9b92f0b0ff096d57c29148ecd12f1727f8
Akiva Bamberger <akiva.bamberger@gmail.com>
2022-12-03 13:08:36 -0800

    Updates

--- script.js
+++ script.js
@@ -44,38 +44,63 @@ const setupBubbles = () => {
 };
 
 const requestOpenAI = (prompt) => {
+    const element = document.getElementById('listening-state');
+    element.innerText = "Thinking...";
+
 // Send an HTTP request to the OpenAI API
-    return fetch("https://api.openai.com/v1/completions", {
+    fetch("https://us-central1-kibo-the-bunny.cloudfunctions.net/requestOpenAPI", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer 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"
         },
         body: JSON.stringify({
-            prompt: prompt,
-            model: "text-davinci-003",
-            max_tokens: 100,
-            temperature: 0.5
+            prompt: prompt
         })
     })
-        .then(response => response.json())
+        .then(res => res.text())
+        .then(res => {
+            speak(res)
+        })
         .catch(error => {
             // Handle any errors that occurred
             console.error(error);
         });
 }
 
-function startRecognizer(firstTime) {
-    const button = document.getElementById('ask-question');
+function speak(text) {
+    if (!isSpeakingWithKibo) {
+        console.log(`Not speaking with KIBO!`)
+        return;
+    }
+    const element = document.getElementById('listening-state');
+    element.innerText = "Speaking...";
+    const utterance = new SpeechSynthesisUtterance(text.replace('\\n', ' '))
+    utterance.addEventListener('end', () => {
+        console.log(`Restarting...`);
+        startRecognizer();
+    })
+    speechSynthesis.speak(utterance);
 
-    const recognizer = new webkitSpeechRecognition();
+}
+
+
+function listen(recognizer) {
+    const element = document.getElementById('listening-state');
+    element.innerText = "Listening...";
+    recognizer.start();
+}
+
+let isSpeakingWithKibo = false;
+function startRecognizer(firstTime) {
+    let recognizer = new webkitSpeechRecognition();
 
     recognizer.lang = 'en-US';
 
     recognizer.continuous = true;
     recognizer.interimResults = true;
     recognizer.addEventListener('result', (event) => {
-        if (!button.hasAttribute('disabled')) {
+        if (!isSpeakingWithKibo) {
+            recognizer.stop();
             return;
         }
         const evt = event.results[0];
@@ -86,40 +111,38 @@ function startRecognizer(firstTime) {
         // Append the transcript element to the body of the page
         if (evt.isFinal) {
             recognizer.stop();
-            requestOpenAI(text).then(res => {
-                const res2 = res.choices[0].text;
-                const utterance = new SpeechSynthesisUtterance(res2.replace('\\n', ' '))
-                utterance.addEventListener('end', () => {
-                    console.log(`Restarting...`);
-                    startRecognizer();
-                })
-                speechSynthesis.speak(utterance);
-            })
+            requestOpenAI(`respond to ${text} and then ask a follow up question`)
         }
     });
 
+    if (!isSpeakingWithKibo) {
+        return;
+    }
+
     if (firstTime) {
-        requestOpenAI("introduce yourself, you're rainbow bunny named Kibo. also remind me to unmute the music by pressing the icon in the top left").then(res => {
-            const res2 = res.choices[0].text;
-            const utterance = new SpeechSynthesisUtterance(res2.replace('\\n', ' '))
-            utterance.addEventListener('end', () => {
-                console.log(`Restarting...`);
-                startRecognizer(false);
-            })
-            speechSynthesis.speak(utterance);
-        })
+        let str = "your name is 'Kibo the rainbow bunny', introduce yourself to me. you're here to listen to me talk"
+        if (document.getElementById('sound-toggle').classList.contains('glowing')) {
+           str += " and also add a reminder that to unmute the music i can press the icon in the top left"
+        }
+        requestOpenAI(str)
     } else {
-        recognizer.start();
+        listen(recognizer)
     }
 }
 const setupAskQuestionButton = () => {
     const button = document.getElementById('ask-question');
 
     button.addEventListener('click', () => {
-        startRecognizer(true);
-
-        button.setAttribute('disabled', '');
-        button.className = 'disabled';
+        if (isSpeakingWithKibo) {
+            isSpeakingWithKibo = false;
+            speechSynthesis.cancel();
+            button.innerText = "Speak to Kibo"
+            document.getElementById('listening-state').innerText = "Paused...";
+        } else {
+            isSpeakingWithKibo = true;
+            startRecognizer(true);
+            button.innerText = "Pause with Kibo"
+        }
     });
 }
 
@@ -129,13 +152,13 @@ const audio = new Audio();
 const playAudio = () => {
     // Create an audio element
 
-// Set the audio source
+    // Set the audio source
     audio.src = "ambient.mp3";
 
-// Set the volume level to 0.5 (half the maximum volume)
+    // Set the volume level to 0.5 (half the maximum volume)
     audio.volume = 0.2;
-
-// Play the audio
+    audio.loop = true;
+    // Play the audio
     audio.play();
 }
 
@@ -146,6 +169,12 @@ const setupToggle = () => {
 
 // Add an event listener to the button
     soundToggle.addEventListener("click", function() {
+        if (soundToggle.classList.contains('glowing')) {
+            if (isSpeakingWithKibo) {
+                requestOpenAI("say something nice about the music and ask me about what's on my mind")
+            }
+        }
+        soundToggle.classList.remove('glowing')
         // Toggle the display of the images
         if (soundOn.style.display === "none") {
             soundOn.style.display = "block";
